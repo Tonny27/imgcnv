c94c0d3aa0d7000d976941f407450988d2546965
Tonny27 <janantonigronowski@gmail.com>
2021-01-19 15:10:34 +0100

    Added base functionality

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -g main.c stb_image.c stb_image_write.c -o imgcnv
+gcc -g main.c -o imgcnv -lm
--- main.c
+++ main.c
@@ -1,12 +1,45 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include "stb_image.h"
+#include "stb_image_write.h"
 
 typedef enum file_type
 {
     NONE, PNG, JPG
 }file_type;
+
+bool WriteToFile(const char* input_file, const char* output_file, file_type input_file_type, file_type output_file_type)
+{
+    int width, height, channels;
+    unsigned char* data = stbi_load(input_file, &width, &height, &channels, (input_file_type == PNG) ? 4 : 3);
+    if (!data)
+    {
+        printf("Error loading file!\n");
+        return false;
+    }
+    switch(output_file_type)
+    {
+    case PNG:
+        stbi_write_png(output_file, width, height, channels, data, width*channels);
+        break;
+    case JPG:
+        stbi_write_jpg(output_file, width, height, channels, data, 100);
+        break;
+    default:
+        printf("Output file type not found!\n");
+        free(data);
+        return false;
+        break;
+    }
+    free(data);
+    return true;
+}
 
 bool GetArgs(int argc, char** argv, char** input_file, char** output_file, file_type* input_file_type, file_type* output_file_type)
 {
@@ -101,6 +134,14 @@ int main(int argc, char** argv)
 
     printf("Input file: %s, output file: %s!\n", input_file, output_file);
     printf("Input file type: %s, output file type: %s!\n", (input_file_type == PNG) ? "png" : "jpg", (output_file_type == PNG) ? "png" : "jpg");
+
+    if (!WriteToFile(input_file, output_file, input_file_type, output_file_type))
+    {
+        printf("Writing failed!\n");
+        free(input_file);
+        free(output_file);
+        return -1;
+    }
 
     free(input_file);
     free(output_file);
